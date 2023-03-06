02af27a05ba39d2c84eef3b8747e9eb29b6f4e87
fboyle2001 <finlayboyle2001@gmail.com>
2023-03-06 11:19:19 +0000

    3D perspective map

--- code/fresh.py
+++ code/fresh.py
@@ -4,6 +4,19 @@ import xarray
 pygmt.config(PROJ_ELLIPSOID="Moon")
 path = "./data/displacement/ldem_4.tif"
 
+def load_displacement_map(pixels_per_degree):
+    loc = f"./data/displacement/ldem_{pixels_per_degree}.tif"
+
+    xds = xarray.open_dataset(loc, engine="rasterio")
+    xds = xds.squeeze()
+
+    xds["x"] = xds["x"] / pixels_per_degree - 180
+    xds["y"] = 90 - xds["y"] / pixels_per_degree
+
+    arr = xds.to_array().squeeze()
+
+    return xds, arr
+
 print(pygmt.grdinfo(path))
 
 """
@@ -11,31 +24,46 @@ Map x: -180 to 180
 Map y: 90 to -90
 """
 
-xds = xarray.open_dataset("./data/displacement/ldem_4.tif", engine="rasterio")
-xds = xds.squeeze()
-
+ppd = 4
+xds, arr = load_displacement_map(ppd)
 print(xds)
-
-xds["x"] = xds["x"] / 4 - 180
-xds["y"] = 90 - xds["y"] / 4
-
-print(xds)
-
-arr = xds.to_array().squeeze()
 
 fig = pygmt.Figure()
-fig.grdimage(
-    grid=arr,
-    cmap="haxby",
-    projection="M10c",
-    frame=True,
-    region=[-180, 180, -85, 85],
-
-)
-# fig.grdcontour(
-#     annotation=1,
-#     interval=1,
-#     grid=arr
+# fig.grdimage(
+#     grid=arr,
+#     cmap="haxby",
+#     projection="M30c",
+#     frame="a30f15",
+#     region=[-0.5, 0, -0.5, 0] #[-180, 180, -70, 70] # type: ignore
 # )
-fig.colorbar(frame=["x+lelevation", "y+lm"])
+
+# Didn't work
+# gradient = pygmt.grdgradient(
+#     grid=arr,
+#     region=[0, 40, -40, 0], # type: ignore
+#     radiance=[135, 30]
+# )
+
+# https://www.pygmt.org/latest/gallery/3d_plots/grdview_surface.html#sphx-glr-gallery-3d-plots-grdview-surface-py
+fig.grdview(
+    grid=arr,
+    region=[0, 40, -40, 0],
+    frame=["a5f1g5", "za5f1g5"],
+    projection="x0.5c",
+    zscale="0.5c",
+    surftype="s",
+    cmap="geo",
+    perspective=[135, 30],
+    shading="+a45",
+    contourpen="1p,blue",
+    # plane="+ggray"
+)
+
+# fig.grdcontour(
+#     grid=arr,
+#     interval=2,
+#     # annotation=5
+# )
+
+fig.colorbar(frame=["x+lelevation", "y+lkm"])
 fig.show()
